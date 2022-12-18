9a65cfd930b0432e69fa35859f1a190fa4da83ad
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-12-18 04:20:47 +0200

    minimal worked example

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Forge } from './forge';
 async function main() {
   await Forge.title('test')
     .addRule('arrows to hjkl')
-    .addManipulator('h')
+    .addManipulator('h to left')
     .setFrom({
       key_code: 'h',
       modifiers: {
@@ -13,10 +13,40 @@ async function main() {
       },
     })
     .setTo({
-      key_code: 'a',
+      key_code: 'left_arrow',
+    })
+    .completeManipulator()
+    .addManipulator('l to right')
+    .setFrom({
+      key_code: 'l',
       modifiers: {
         mandatory: ['left_command'],
       },
+    })
+    .setTo({
+      key_code: 'left_arrow',
+    })
+    .completeManipulator()
+    .addManipulator('j to down')
+    .setFrom({
+      key_code: 'j',
+      modifiers: {
+        mandatory: ['left_command'],
+      },
+    })
+    .setTo({
+      key_code: 'down_arrow',
+    })
+    .completeManipulator()
+    .addManipulator('k to up')
+    .setFrom({
+      key_code: 'k',
+      modifiers: {
+        mandatory: ['left_command'],
+      },
+    })
+    .setTo({
+      key_code: 'up_arrow',
     })
     .completeManipulator()
     .completeRule()
